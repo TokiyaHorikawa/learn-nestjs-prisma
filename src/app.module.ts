19cd09575c6df74f3460195f43a1fb56f92ee66e
tokiya.horikawa <ayikot4423@gmail.com>
2022-09-19 20:47:58 +0900

    feat: Add grahql post schema and resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsResolver } from './posts/posts.resolver';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, PostsResolver],
 })
 export class AppModule {}
